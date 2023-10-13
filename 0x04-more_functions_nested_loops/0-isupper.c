e7e4ad2d1d9ca1694b9168c6e6eab5ec622319e8
theBeverlyS <beverlyy.sam@gmail.com>
2023-10-12 17:04:35 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,16 @@
 
 /**
  * _isupper - uppercase letters
- * @c: char checking uppercase
+ * @c: char to check
  *
- * Return 1 otherwise 0
+ * Return: 0 or 1
  */
+
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
 }
+
