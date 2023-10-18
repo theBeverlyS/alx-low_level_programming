70dbd42da898f4c0c9fc7ef328c954954f76e08e
theBeverlyS <beverlyy.sam@gmail.com>
2023-10-18 03:43:15 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,6 +6,7 @@
  * @n: is the number of elements OF the array to be printed
  * Return: a and n inputs
  */
+
 void print_array(int *a, int n)
 {
 	int i;
@@ -20,4 +21,3 @@ void print_array(int *a, int n)
 		}
 			printf("\n");
 }
-
