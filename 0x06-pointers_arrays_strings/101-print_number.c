c4607cab1cb278ffdcfd952eeb9b8d73aa2fd870
theBeverlyS <beverlyy.sam@gmail.com>
2023-10-20 17:33:01 -0700

    Advanced tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
+ * print_number - main function
+ *
+ * @n: Function parameter
+ *
+ * Return: Always 0.
  */
 
 void print_number(int n)
@@ -14,13 +16,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n1 = -n;
+	_putchar('-');
+	n1 = -n;
 	}
 
 	if (n1 / 10 != 0)
 	{
-		print_number(n1 / 10);
+	print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
