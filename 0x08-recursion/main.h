1c95774823b2a1efb967e9584c8bdd8d6fb2583c
theBeverlyS <beverlyy.sam@gmail.com>
2023-10-25 09:21:36 -0700

    Main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -10,5 +9,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
 #endif
