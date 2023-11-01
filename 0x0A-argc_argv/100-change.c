d6e4c7a40d0673ccd4151d900699b1462d68b81f
theBeverlyS <beverlyy.sam@gmail.com>
2023-11-01 15:31:24 -0700

    Advanced task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,50 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * main - prints the minimum number of coins to
- * make change for an amount of money
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
-
-int main(int argc, char *argv[])
-
+* main - prints the min number of coins to make change
+* for an amount of money
+* @argc: argument count
+* @argv: arguments
+* Return: 0
+*/
+int main(int argc, char **argv)
 {
-	int num, j, result;
-	int coins[] = {25, 10, 5, 2, 1};
-	if (argc != 2)
+int total, count;
+unsigned int i;
+char *p;
+int cents[] = {25, 10, 5, 2};
 
-	{
-		printf("Error\n");
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-		return (1);
-	}
+total = strtol(argv[1], &p, 10);
+count = 0;
 
-	num = atoi(argv[1]);
-	result = 0;
+if (!*p)
+{
+while (total > 1)
+{
+for (i = 0; i < sizeof(cents[i]); i++)
+{
+if (total >= cents[i])
 
-	if (num < 0)
+count += total / cents[i];
+total = total % cents[i];
 
-	{
-		printf("0\n");
+}
+}
+if (total == 1)
+count++;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
 
-		return (0);
 
-	}
-
-	for (j = 0; j < 5 && num >= 0; j++)
-
-	{
-		while (num >= coins[j])
-		{
-			result++;
-			num -= coins[j];
-		}
-	}
-	printf("%d\n", result);
-	return (0);
+printf("%d\n", count);
+return (0);
 }
